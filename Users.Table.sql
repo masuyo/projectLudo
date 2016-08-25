be0c54d7e273111c51ca1975745d982ea8b2a647
Sinka-Pálinkás Gabriella <fusewent@gmail.com>
2016-08-25 15:23:59 +0200

    UsersModel

---
+++ Users.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [EmailID] VARCHAR(128) NULL
+)
