7919d469bfb3042c3d296e18fec3ddd08438bb3f
Sinka-Pálinkás Gabriella <fusewent@gmail.com>
2016-08-25 15:54:50 +0200

    Register metódus felhasználó adatbázishoz adásához

---
+++ pageLudo/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pageLudo.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Users u)
+        {
+            if (ModelState.IsValid)
+            {
+                // hozzáadja a felhasználót az adatbázishoz
+                using (UserDatabaseEntities ude = new UserDatabaseEntities())
+                {
+                    ude.Users.Add(u);
+                    ude.SaveChanges();
+                    ModelState.Clear();
+                    u = null;
+                    ViewBag.Message = "Registration done";
+                }
+            }
+            return View(u);
+        }
+    }
+}
