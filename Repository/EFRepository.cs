306ad59b2766e5b7d8bb7926207c0efa55d3e89c
Cser_Zoltán <cszoolee@gmail.com>
2016-08-27 13:52:24 +0200

    IRepository +  abstract EFRepository UsersRepository

---
+++ Repository/EFRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    abstract public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext context;
+
+        public EFRepository(DbContext newctx)
+        {
+            context = newctx;
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            context.Set<TEntity>().Remove(entityToDelete);
+            context.Entry(entityToDelete).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            TEntity entityToDelete = GetById(id);
+            if (entityToDelete == null) throw new ArgumentException(String.Format("No data with id: {0}", id));
+            Delete(entityToDelete);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public IQueryable<TEntity> Get(System.Linq.Expressions.Expression<Func<TEntity, bool>> filter)
+        {
+            return GetAll().Where(filter);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return context.Set<TEntity>();
+        }
+
+        abstract public TEntity GetById(int id);
+        
+
+        public void Insert(TEntity newentity)
+        {
+            context.Set<TEntity>().Add(newentity);
+            context.Entry(newentity).State = EntityState.Added;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Repository/TableRepositories/UsersRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entities;
+
+namespace Repository.TableRepositories
+{
+    public class UsersRepository : EFRepository<users>
+    {
+        public UsersRepository(DbContext newctx) : base(newctx)
+        {
+        }
+
+        public override users GetById(int id)
+        {
+            return Get(akt => akt.id == id).SingleOrDefault();
+        }
+    }
+}
